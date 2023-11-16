ee33d9d4df8652644bfe89f39030e7e8e5ca0846
OVO <lzx213@sina.com>
2023-11-16 15:04:26 -0800

    add enemy animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    Animator animator;
+    [Header("Enemy Movement parameters")]
+    public float normalSpeed;
+    public float chaseSpeed;
+    public float currentSpeed;
+
+    public Vector3 faceDirection;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentSpeed = normalSpeed;
+    }
+
+    private void Update()
+    {
+        faceDirection = new Vector3(-transform.localScale.x, 0, 0);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(currentSpeed * Time.deltaTime * faceDirection.x, rb.velocity.y);
     }
 }
