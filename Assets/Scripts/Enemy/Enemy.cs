ab8129e0e52d1aa3eed549e56d8acf582746c81c
Zexuan <lzx213@sina.com>
2023-11-16 21:30:33 -0800

    add waiting state of boar

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
     Rigidbody2D rb;
     PhysicsCheck physicsCheck;
     protected Animator animator;
@@ -13,6 +14,14 @@ public class Enemy : MonoBehaviour
     public float currentSpeed;
 
     public Vector3 faceDirection;
+
+    protected Transform attacker;
+
+    [Header("Timer")]
+    public float waitTime;
+    public float waitTimeCounter;
+    public bool wait;
+
 
     private void Awake()
     {
@@ -25,11 +34,15 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         faceDirection = new Vector3(-transform.localScale.x, 0, 0);
-        if(physicsCheck.touchLeftWall || physicsCheck.touchRightWall)
+
+        if ((physicsCheck.touchLeftWall && faceDirection.x < 0) || (physicsCheck.touchRightWall && faceDirection.x > 0))
         {
-            transform.localScale = new Vector3(faceDirection.x, 1, 1);
+            wait = true;
+            animator.SetBool("walk", false);
         }
-        
+
+        TimeCounter();
+
     }
 
     private void FixedUpdate()
@@ -40,5 +53,34 @@ public class Enemy : MonoBehaviour
     public virtual void Move()
     {
         rb.velocity = new Vector2(currentSpeed * Time.deltaTime * faceDirection.x, rb.velocity.y);
+    }
+
+    public void TimeCounter()
+    {
+        if (wait)
+        {
+            waitTimeCounter -= Time.deltaTime;
+            if (waitTimeCounter <= 0)
+            {
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDirection.x, 1, 1);
+            }
+        }
+    }
+
+    public void OnTakenDamage(Transform attackTrans)
+    {
+        attacker = attackTrans;
+
+        //Turn to face attacker
+        if(attackTrans.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(attackTrans.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
